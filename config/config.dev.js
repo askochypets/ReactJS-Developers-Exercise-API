89a89ec6891491155dacf3a89cbcb94149c4ca0f
Andrii Skochypets <askochypets@intellias.com>
2019-07-18 10:52:45 +0300

    update build script

--- config/config.dev.js
+++ config/config.dev.js
@@ -1,5 +1,6 @@
 export default {
-  MONGO_URI: 'mongodb://localhost:27017/Shippingdb',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/Shippingdb',
   serverPort: process.env.serverPort || 3001,
-  YAddress_BASE_URL: 'http://www.yaddress.net/api/address',
+  YAddress_BASE_URL:
+    process.env.YAddress_BASE_URL || 'http://www.yaddress.net/api/address',
 };
